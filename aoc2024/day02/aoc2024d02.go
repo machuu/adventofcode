7db7ab37d66eb0b80632e24afcd06e5636bdfabb
Matt Hoover <matthew.hoover@sony.com>
2024-12-05 11:48:54 -0500

    Day 02 Part 2: Update README and extract safe logic

--- aoc2024/day02/aoc2024d02.go
+++ aoc2024/day02/aoc2024d02.go
@@ -10,49 +10,56 @@ import (
 func countSafeReports(reports [][]int) int {
 	safeReports := 0
 	for j, report := range reports {
-		safe := true
-		increased := false
-		decreased := false
-
-		// get report as string
-		reportJson, _ := json.Marshal(report)
-		reportString := string(reportJson)
-		utils.Use(j, reportString)
-		for i := range len(report) {
-			if i == len(report)-1 {
-				break
-			}
-
-			currentLevel := report[i]
-			nextLevel := report[i+1]
-			//fmt.Printf("Checking levels [%d]: %d -> %d\n", i, report[i], report[i+1])
-			diff := utils.AbsDiffInt(currentLevel, nextLevel)
-
-			if currentLevel > nextLevel {
-				decreased = true
-			} else {
-				increased = true
-			}
-
-			if increased && decreased {
-				//fmt.Printf("Report [%d] is unsafe: inscreased and decreased in %s\n", j, reportString)
-				safe = false
-				break
-			} else if diff > 3 {
-				//fmt.Printf("Report [%d] is unsafe: |%d-%d| > 3\n", j, report[i], report[i+1])
-				safe = false
-				break
-			} else if diff == 0 {
-				//fmt.Printf("Report [%d] is unsafe: |%d-%d| = 0\n", j, report[i], report[i+1])
-				safe = false
-				break
-			}
-		}
-		if safe {
+		utils.Use(j)
+		if isReportSafe(report) {
 			safeReports += 1
 		}
 	}
 	return safeReports
+}
+
+func isReportSafe(report []int) bool {
+	// get report as string
+	reportJson, _ := json.Marshal(report)
+	reportString := string(reportJson)
+	utils.Use(reportString)
+
+	safe := true
+	increased := false
+	decreased := false
+
+	for i := range len(report) {
+		if i == len(report)-1 {
+			break
+		}
+
+		currentLevel := report[i]
+		nextLevel := report[i+1]
+		//fmt.Printf("Checking levels [%d]: %d -> %d\n", i, report[i], report[i+1])
+		diff := utils.AbsDiffInt(currentLevel, nextLevel)
+
+		if currentLevel > nextLevel {
+			decreased = true
+		} else {
+			increased = true
+		}
+
+		if increased && decreased {
+			//fmt.Printf("Report is unsafe: inc and dec -> %s\n", j, reportString)
+			safe = false
+			break
+		} else if diff > 3 {
+			//fmt.Printf("Report is unsafe: |%d-%d| > 3 -> %s\n", j, currentLevel, nextLevel)
+			safe = false
+			break
+		} else if diff == 0 {
+			//fmt.Printf("Report is unsafe: |%d-%d| = 0 -> %s\n", j, currentLevel, nextLevel)
+			safe = false
+			break
+		}
+	}
+
+	return safe
 }
 
 func SolnPart1() {
