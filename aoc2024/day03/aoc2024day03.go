ad5ed590a8343522756708fe62a04a3ca457670a
Matt Hoover <matthew.hoover@sony.com>
2024-12-09 22:54:55 -0600

    Day 03 Part 2: Solved

--- aoc2024/day03/aoc2024day03.go
+++ aoc2024/day03/aoc2024day03.go
@@ -13,14 +13,30 @@ type mul struct {
 
 func mulStringsToMuls(mulStrings []string) []mul {
 	var muls []mul
+	do := true
+	var command string
 	for _, mulString := range mulStrings {
 		var num1 int
 		var num2 int
-		fmt.Sscanf(mulString, "mul(%d,%d)", &num1, &num2)
-		fmt.Printf("extracting mul: '%s' -> (%d,%d)\n", mulString, num1, num2)
-		muls = append(muls, mul{num1, num2})
+		nNums, _ := fmt.Sscanf(mulString, "mul(%d,%d)", &num1, &num2)
+		if nNums == 0 {
+			nCommand, _ := fmt.Sscanf(mulString, "%s", &command)
+			if nCommand == 0 {
+				// shrug
+			} else if command == "do()" {
+				do = true
+			} else if command == "don't()" {
+				do = false
+			} else {
+				fmt.Printf("Unrecognized command: %s\n", command)
+			}
+			continue
+		}
+		//fmt.Printf("extracting mul: '%s' -> (%d,%d)\n", mulString, num1, num2)
+		if do {
+			muls = append(muls, mul{num1, num2})
+		}
 	}
-
 	return muls
 }
 
@@ -38,9 +54,17 @@ func SolnPart1() {
 	mulStrings := utils.GetRegexMatches(`mul\(\d{1,3},\d{1,3}\)`, "./day03/input.dat")
 	muls := mulStringsToMuls(mulStrings)
 	results := multiplyMuls(muls)
-	fmt.Printf("Day 03 Part 1: %d", results)
+	fmt.Printf("Day 03 Part 1: %d\n", results)
+}
+
+func SolnPart2() {
+	mulStrings := utils.GetRegexMatches(`(mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\))`, "./day03/input.dat")
+	muls := mulStringsToMuls(mulStrings)
+	results := multiplyMuls(muls)
+	fmt.Printf("Day 03 Part 2: %d\n", results)
 }
 
 func Solution() {
 	SolnPart1()
+	SolnPart2()
 }
